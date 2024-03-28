c3ad28bd85f00c9a80d1a822666c1f365bff9aa2
jgroma <jgromadzinska9@gmail.com>
2024-03-28 17:03:14 +0000

    Connect to database, practice SQLite CRUD

--- App.tsx
+++ App.tsx
@@ -7,54 +7,125 @@ import * as FileSystem from "expo-file-system";
 import { SQLiteProvider, useSQLiteContext } from "expo-sqlite/next";
 import Testing from "./components/Testing";
 
-async function loadDatabase() {
-  const dbName = "shepherd.db";
-  const dbAsset = require("./assets/shepherd.db");
-  const dbUri = Asset.fromModule(dbAsset).uri;
-  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+// async function loadDatabase() {
+//   const dbName = "shepherd.db";
+//   const dbAsset = require("./assets/shepherd.db");
+//   const dbUri = Asset.fromModule(dbAsset).uri;
+//   const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
 
-  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
-  if (!fileInfo.exists) {
-    console.log("hello");
+//   const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+//   if (!fileInfo.exists) {
+//     console.log("hello");
 
-    await FileSystem.makeDirectoryAsync(
-      `${FileSystem.documentDirectory}SQLite`,
-      { intermediates: true }
-    );
-    await FileSystem.downloadAsync(dbUri, dbFilePath);
-  }
-}
+//     await FileSystem.makeDirectoryAsync(
+//       `${FileSystem.documentDirectory}SQLite`,
+//       { intermediates: true }
+//     );
+//     await FileSystem.downloadAsync(dbUri, dbFilePath);
+//   }
 
+//  const db = SQLite.openDatabase("shepherd.db");
+
+//  db.TransactionAsync(async () => {
+//   await db.runAsync(
+//     `CREATE TABLE Sheep IF DOES NOT EXISTS (sheep_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR, description VARCHAR);
+//     `,
+//     []
+//   );
+//   console.log("table created???");
+// });
+// }
 
 export default function App() {
   // const db = useSQLiteContext()
-  const dbAsset = require("./assets/shepherd.db");
-  const db = dbAsset
+  // const dbAsset = require("./assets/shepherd.db");
+  // const db = dbAsset;
+
+  // useEffect(() => {
+  //   loadDatabase().then(() => {
+  //     db.withTransactionAsync(async () => {
+  //       await db.runAsync(
+  //         `CREATE TABLE Sheep IF DOES NOT EXISTS (sheep_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR, description VARCHAR);
+  //         `,
+  //         []
+  //       );
+  //       console.log("table created???");
+  //     });
+  //   });
+  // }, []);
+  // async function createTable(database: any) {
+  //   const readOnly = true;
+  //   await database.transactionAsync(async (tx: any) => {
+  //     const result = await tx.executeSqlAsync(
+  //       `CREATE TABLE IF NOT EXISTS Sheep (
+  //         sheep_id INTEGER PRIMARY KEY,
+  //         name VARCHAR(50),
+  //         description VARCHAR(50),
+  //         personality VARCHAR(50),
+  //         joke_category VARCHAR(50),
+  //         status VARCHAR(50),
+  //         skin VARCHAR(50),
+  //         frequency INT,
+  //         health_timer INT,
+  //         days_since_fed DATE,
+  //         death_date DATE
+  //     );`
+  //     );
+  //     // console.log("Count:", result.rows[0]["COUNT(*)"])
+  //     console.log("hii");
+  //   }, readOnly);
+  // }
+
+  function createTable(database: any) {
+    const readOnly = true;
+    database.transaction((tx: any) => {
+      const result = tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS Sheep (
+          sheep_id INTEGER PRIMARY KEY,
+          name VARCHAR(50),
+          description VARCHAR(50),
+          personality VARCHAR(50),
+          joke_category VARCHAR(50),
+          status VARCHAR(50),
+          skin VARCHAR(50),
+          frequency INT,
+          health_timer INT,
+          days_since_fed DATE,
+          death_date DATE
+      );`
+      );
+      // console.log("Count:", result.rows[0]["COUNT(*)"])
+      console.log("hii");
+    }, readOnly);
+  }
+
+  const db = SQLite.openDatabase("shepherd.db");
 
   useEffect(() => {
-    loadDatabase().then(() => {});
+    // createTable(db).then(() => {
+    //   console.log("table created");
+    // });
+    createTable(db);
   }, []);
-  
-  const setGoals = async () => {
-    db.withTransactionAsync(async () => {
-      await db.runAsync(
-        `INSERT INTO Sheep (name, description) VALUES (?, ?)
-        `, ["babaa", "Pass interview at Wiley Edge"]
-      )
-      console.log("sheep added");
-      
-    })
-  };
+
+  // const setGoals = async (db) => {
+  //   db.transaction(async () => {
+  //     await db.executeSQLAsync(
+  //       `INSERT INTO Sheep (name, description) VALUES (?, ?)
+  //       `,
+  //       ["babaa", "Pass interview at Wiley Edge"]
+  //     );
+  //     console.log("sheep added");
+  //   });
+  // };
 
   return (
     <>
       <SQLiteProvider databaseName="shepherd.db">
         <View style={styles.container}>
-          <Text>
-            Hello world
-          </Text>
-        <Testing />
-      
+          <Text>Hello world</Text>
+          <Testing />
+
           <StatusBar style="auto" />
         </View>
       </SQLiteProvider>
--- components/Testing.tsx
+++ components/Testing.tsx
@@ -4,6 +4,7 @@ import { SQLiteProvider, useSQLiteContext } from "expo-sqlite/next";
 
 export default function Testing() {
   const db = useSQLiteContext();
+  console.log(db);
   const setGoals = async () => {
     db.withTransactionAsync(async () => {
       await db.runAsync(
@@ -15,17 +16,48 @@ export default function Testing() {
     });
   };
 
-  const getSheep = async () => {
+  const deleteSheep = async () => {
     db.withTransactionAsync(async () => {
-      const result = await db.runAsync(`SELECT * FROM Sheep;`);
-      console.log(result);
+      await db.runAsync(`DELETE FROM Sheep where sheep_id = ?`, [4]);
     });
+  };
+
+  // const getSheep = async () => {
+  //   // db.withTransactionAsync(async () => {
+  //   //   const result = await db.runAsync(`SELECT * FROM Sheep;`);
+  //   //   console.log(result);
+  //   // });
+  //   // const readOnly = true;
+  //   // await db.withTransactionAsync(async (tx) => {
+  //   //   const result = await tx.executeAsync("SELECT * FROM Sheep", true);
+  //   //   // console.log('Count:', result.rows[0]['COUNT(*)']);
+  //   //   console.log(result);
+  //   //   console.log("???");
+  //   });
+  // };
+
+  const getSheep = async (db: any) => {
+    const query = `SELECT * FROM Sheep`;
+    try {
+      const results = await db.getAllAsync(query);
+      // if (results[0]?.rows?.length) {
+      //   return results[0].rows.item(0)
+      // } else {
+      //   return null
+      // }
+      console.log(results);
+      return results;
+    } catch (error) {
+      console.error(error);
+      throw Error("Failed to get sheep from database");
+    }
   };
 
   return (
     <View>
-      <Button title="getsheep" onPress={getSheep} />
+      <Button title="getsheep" onPress={() => getSheep(db)} />
       <Button title="goals" onPress={setGoals} />
+      <Button title="delete" onPress={deleteSheep} />
     </View>
   );
 }
